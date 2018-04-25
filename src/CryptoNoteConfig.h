50a8304826ce83b0b06aa82a1c9e6dce8cc036d7
The-Stefche <35749934+The-Stefche@users.noreply.github.com>
2018-04-25 16:12:57 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Stefche";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f0b7f28d8e01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f72f253b2feac3c241b829fc50e7fae824b2d669c22e54f910fe768de7e91e33";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f0b7f28d8e01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101bd433946f0a5535d1eb2dc01b878c53403e1643f28516cb0e2a7e702878245f4";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "013c01ff0001f0b7
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "34.238.80.194:3841",
-  "54.161.179.80:3841" ,
+  "107.23.254.24:3841",
+  "35.170.185.87:3841" ,
 };
 
 struct CheckpointData {
